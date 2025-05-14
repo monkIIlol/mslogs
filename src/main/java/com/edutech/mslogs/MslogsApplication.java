6bb4bcc90d3d5651883e4458a038ec4c39458550
Cristian Urcullu <cr.urcullu@duocuc.cl>
2025-05-14 12:51:51 -0400

    first commit

---
+++ src/main/java/com/edutech/mslogs/MslogsApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.mslogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MslogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MslogsApplication.class, args);
+	}
+
+}
