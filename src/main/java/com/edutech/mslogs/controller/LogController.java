fde221238e1d1ca4df4758902871f63ccf42a250
Cristian Urcullu <cr.urcullu@duocuc.cl>
2025-05-28 13:30:26 -0400

    find Usuario

--- src/main/java/com/edutech/mslogs/controller/LogController.java
+++ src/main/java/com/edutech/mslogs/controller/LogController.java
@@ -1,5 +1,6 @@
 package com.edutech.mslogs.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +46,21 @@ public class LogController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("usuario/log/{idUsuario}")
+    public ResponseEntity<List<Log>> getListLog(@PathVariable int idUsuario) {
+        List<Log> buscar = logService.findAll(); 
+            List<Log>  listaLog = new ArrayList<>();
+            for (Log log : buscar) {
+                if(log.getIdUsuario() == idUsuario) {
+                    listaLog.add(log);
+                }
+            
+            }
+            return new ResponseEntity<>(listaLog, HttpStatus.OK);
+        }
+    
+    
 
     @PostMapping
     public ResponseEntity<Log> postPaciente(@RequestBody Log log) {
